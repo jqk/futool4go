77cb9c2654d5e0ee08fc3c330463148db48f9e50
Jason <service_register@126.com>
2023-08-02 23:03:29 +0800

    parseDateTime() ready

--- timeutils/timeparser.go
+++ timeutils/timeparser.go
@@ -6,11 +6,14 @@ import (
 	"time"
 )
 
+// 用于 UnixTime 的正则表达式：
+// 1. 可以有字符前缀及后缀。
+// 2. 需要至少 10 位数字代表秒数。
+// 3. 可以再有 3 位数字代表毫秒数。不足不算。
+var regexUnixTime = regexp.MustCompile(`^.*?(\d{1,10})(\d{3})?.*`)
+
 func ParseUnixTime(s string) *time.Time {
-	// 前后都可以有字符，但需要至少 10 位数字代表秒数。
-	// 可以再有 3 位数字代表毫秒数。
-	re := regexp.MustCompile(`^.*?(\d{1,10})(\d{3})?.*`)
-	subs := re.FindStringSubmatch(s)
+	subs := regexUnixTime.FindStringSubmatch(s)
 	count := len(subs)
 
 	if count <= 1 {
@@ -19,15 +22,90 @@ func ParseUnixTime(s string) *time.Time {
 	}
 
 	// 第 1 个匹配是 10 位，代表秒数。到此处必定存在。
-	var nanoseconds int64 = 0
-	seconds, _ := strconv.ParseInt(subs[1], 10, 64)
+	var nanosecond int64 = 0
+	second, _ := strconv.ParseInt(subs[1], 10, 64)
 
 	// 第 2 个匹配是 3 位，代表毫秒数，要转换为纳秒。可能不存在。
 	if count > 2 {
-		nanoseconds, _ = strconv.ParseInt(subs[2], 10, 64)
-		nanoseconds *= 1000_000
+		nanosecond, _ = strconv.ParseInt(subs[2], 10, 64)
+		nanosecond *= 1000_000
 	}
 
-	result := time.Unix(seconds, nanoseconds)
+	result := time.Unix(second, nanosecond)
+	return &result
+}
+
+// 用于无分隔符的日期时间正则表达式：
+//  1. 可以有字符前缀及后缀。
+//  2. 日期数字之间无分隔符，需要至少 8 位数字表示 YYYYMMDD。
+//  3. 日期与时间之间可以有为分隔符，也可以无分隔符。分隔符可以是“_”、“-”、“.”、“ ”和“T”。
+//  4. 时间数字之间无分隔符，可以是 4 位数字，6 位数字，或者 9 位数字。
+//     分别表示 HHMM，HHMMSS 及 HHMMSSSS。也就是说，可以精确到分钟、秒或毫秒。
+//  5. 毫秒数为 3 位，与秒数之间可以有“.”作为分隔符，也可以无分隔符。
+var regexDateTimeNoSep = regexp.MustCompile(
+	`^.*?(\d{4})(\d{2})(\d{2})[-|_|\.| |T]?` +
+		`(\d{2})(\d{2})((\d{2})\.?(\d{3})?)?.*`)
+
+// 用于有分隔符的日期时间正则表达式：
+//  1. 可以有字符前缀及后缀。
+//  2. 日期数字之间有分隔符，年 4 位，月 1 或 2 位，日 1 或 2 位。
+//  3. 日期与时间之间必须有为分隔符，可以是“_”、“-”、“.”、“ ”和“T”。
+//  4. 时间数字之间有分隔符，可以是“_”、“-”、“.”、“:”。秒与毫秒之间只能是“.”。
+//     小时 1 或 2 位，分钟和秒都是 2 位，毫秒是 3 位。可以精确到分钟、秒或毫秒。
+var regexDateTimeHasSep = regexp.MustCompile(
+	`^.*?(\d{4})[-|_|\.|](\d{1,2})[-|_|\.|](\d{1,2})[-|_|\.| |T]` +
+		`(\d{1,2})[-|_|\.|\:|](\d{2})([-|_|\.|\:|](\d{2})(\.(\d{3}))?)?.*`)
+
+func ParseDateTime(s string) *time.Time {
+	result := parseDateTimeHasSeperator(s)
+	if result != nil {
+		return result
+	}
+
+	return parseDateTimeNoSeperator(s)
+}
+
+func parseDateTimeNoSeperator(s string) *time.Time {
+	subs := regexDateTimeNoSep.FindStringSubmatch(s)
+	if len(subs) == 0 {
+		// 没有配置的日期时间字符串，所以数组长度为 0，返回 nil 说明转换不成功。
+		return nil
+	}
+
+	year, _ := strconv.Atoi(subs[1])
+	m, _ := strconv.Atoi(subs[2])
+	month := time.Month(m)
+	day, _ := strconv.Atoi(subs[3])
+	hour, _ := strconv.Atoi(subs[4])
+	minute, _ := strconv.Atoi(subs[5])
+	// subs[6] 包含了秒和毫秒。
+	second, _ := strconv.Atoi(subs[7])
+	millisecond, _ := strconv.Atoi(subs[8])
+	nanosecond := millisecond * 1000_000
+
+	result := time.Date(year, month, day, hour, minute, second, nanosecond, time.Local)
+	return &result
+}
+
+func parseDateTimeHasSeperator(s string) *time.Time {
+	subs := regexDateTimeHasSep.FindStringSubmatch(s)
+	if len(subs) == 0 {
+		// 没有配置的日期时间字符串，所以数组长度为 0，返回 nil 说明转换不成功。
+		return nil
+	}
+
+	year, _ := strconv.Atoi(subs[1])
+	m, _ := strconv.Atoi(subs[2])
+	month := time.Month(m)
+	day, _ := strconv.Atoi(subs[3])
+	hour, _ := strconv.Atoi(subs[4])
+	minute, _ := strconv.Atoi(subs[5])
+	// subs[6] 包含了秒和毫秒。
+	second, _ := strconv.Atoi(subs[7])
+	// subs[8] 包含了"."和毫秒。
+	millisecond, _ := strconv.Atoi(subs[9])
+	nanosecond := millisecond * 1000_000
+
+	result := time.Date(year, month, day, hour, minute, second, nanosecond, time.Local)
 	return &result
 }
--- timeutils/timeparser_test.go
+++ timeutils/timeparser_test.go
@@ -8,16 +8,18 @@ import (
 )
 
 func TestParseUnixTime(t *testing.T) {
-	// 数字是 13 位，有秒有毫秒。
+	// 数字是 13 位，有秒有毫秒。尾部再长，超过 13 位，将被忽略。
 	tm := ParseUnixTime("snapshot_1553867509757.png")
+	assert.NotNil(t, tm)
 	assert.Equal(t, int64(1553867509757), tm.UnixMilli())
 
 	expect, err := time.ParseInLocation("2006-01-02T15:04:05.000", `2019-03-29T21:51:49.757`, time.Local)
 	assert.Nil(t, err)
 	assert.Equal(t, expect, *tm)
 
-	// 由于仅 12 位，所以毫秒数不够 3 位，不取毫秒。
+	// 由于仅 12 位，所以毫秒数不够 3 位，不取毫秒，因此只处理 10 位。
 	tm = ParseUnixTime("155386750975abcd")
+	assert.NotNil(t, tm)
 	assert.Equal(t, int64(1553867509000), tm.UnixMilli())
 
 	// 后缀 Z 表示 UTC 时间。
@@ -27,4 +29,58 @@ func TestParseUnixTime(t *testing.T) {
 	// 必须转换一下时区，虽然即使不转换，绝对时间点也相同。但不转的话，比较时会报错。
 	expect = expect.In(time.Local)
 	assert.Equal(t, expect, *tm)
+}
+
+func TestParseDateTimeNoSeperator(t *testing.T) {
+	// 有前、后缀，“-”作为分隔符，精确到分钟的字符串。
+	tm := ParseDateTime("abc20100223-1534ddd.jpg")
+	assert.NotNil(t, tm)
+	assert.Equal(t, "2010-02-23 15:34:00", tm.Format("2006-01-02 15:04:05"))
+
+	// 无前缀、有后缀，“.”作为分隔符，精确到毫秒的字符串。
+	tm = ParseDateTime("20100223.153456.789ddd.jpg")
+	assert.NotNil(t, tm)
+	assert.Equal(t, "2010-02-23 15:34:56.789", tm.Format("2006-01-02 15:04:05.000"))
+
+	// 无前缀、无后缀，无分隔符，精确到毫秒的字符串。
+	tm = ParseDateTime("20100223153456789")
+	assert.NotNil(t, tm)
+	assert.Equal(t, "2010-02-23 15:34:56.789", tm.Format("2006-01-02 15:04:05.000"))
+
+	// 有前缀、无后缀，空格作为无分隔符，精确到秒的字符串。
+	// 注意，最后一位”7“由于在秒之后，又组不成 3 位毫秒，所以被忽略。
+	tm = ParseDateTime("20100223 1534567")
+	assert.NotNil(t, tm)
+	assert.Equal(t, "2010-02-23 15:34:56", tm.Format("2006-01-02 15:04:05"))
+
+	// 无效的字符串。
+	tm = ParseDateTime("20100223")
+	assert.Nil(t, tm)
+}
+
+func TestParseDateTimeHasSeperator(t *testing.T) {
+	// 有前、后缀，精确到分钟的字符串。
+	tm := ParseDateTime("abc2010-02-23-15:34ddd.jpg")
+	assert.NotNil(t, tm)
+	assert.Equal(t, "2010-02-23 15:34:00", tm.Format("2006-01-02 15:04:05"))
+
+	// 无前缀、有后缀，精确到毫秒的字符串。注意月份和小时都只有 1 位。
+	tm = ParseDateTime("2010_2-23.5.34.56.789ddd.jpg")
+	assert.NotNil(t, tm)
+	assert.Equal(t, "2010-02-23 05:34:56.789", tm.Format("2006-01-02 15:04:05.000"))
+
+	// 无前缀、无后缀，精确到毫秒的字符串。
+	tm = ParseDateTime("2010.02.23T15-34_56.789")
+	assert.NotNil(t, tm)
+	assert.Equal(t, "2010-02-23 15:34:56.789", tm.Format("2006-01-02 15:04:05.000"))
+
+	// 有前缀、无后缀，精确到秒的字符串。
+	// 注意，最后一位”7“由于在秒之后，又组不成 3 位毫秒，所以被忽略。
+	tm = ParseDateTime("2010-02-23 15:34:56.7")
+	assert.NotNil(t, tm)
+	assert.Equal(t, "2010-02-23 15:34:56", tm.Format("2006-01-02 15:04:05"))
+
+	// 无效的字符串。
+	tm = ParseDateTime("2010-02-23")
+	assert.Nil(t, tm)
 }
