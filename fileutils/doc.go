75cc6a3fd9a6ae6b75f232f8a336ce5daa0f77b4
Jason <service_register@126.com>
2023-08-07 14:40:37 +0800

    包说明

---
+++ fileutils/doc.go
@@ -0,0 +1,7 @@
+/*
+fileutils provides functions for calculating file checksums,
+statistics for file extensions and file filtering functions.
+
+fileutils 提供计算文件检验值的函数、统计文件扩展名的函数和文件过滤函数。
+*/
+package fileutils
---
+++ timeutils/doc.go
@@ -0,0 +1,14 @@
+/*
+timeutils provides a set of time handling functions.
+
+[Stopwatch] is a timer, thread safe.
+
+Time string parsing functions, the returned time zone is time.Local.
+
+timeutils 提供一组时间处理函数。
+
+[Stopwatch] 计时器，多线程安全。
+
+时间字符解析函数，返回的时区都是 time.Local。
+*/
+package timeutils
